5df8cd1f5e51218ed0ce428a07438070ef1d82db
jujinyoung <jujinyoung1838@naver.com>
2024-03-13 11:07:49 +0900

    Nuxt Modules - i18n 설치

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,93 +2,99 @@
   <q-layout view="hHh lpR fFf" class="bg-grey-2">
     <q-header elevated class="bg-dark text-white">
       <q-toolbar>
-<!--        <ClientOnly>-->
-          <q-toolbar-title>Vue Master Course</q-toolbar-title>
-          <q-separator dark vertical />
-          <NuxtLink v-slot="{ navigate }" custom to="/">
-            <q-btn stretch flat :label="$t('home')" no-caps @click="navigate()" />
-          </NuxtLink>
-          <q-separator dark vertical />
-          <NuxtLink v-slot="{ navigate }" custom to="/about">
-            <q-btn
-                stretch
-                flat
-                :label="$t('about')"
-                no-caps
-                @click="navigate()"
-            />
-          </NuxtLink>
-          <q-separator dark vertical />
+        <q-toolbar-title>Vue Master Course</q-toolbar-title>
+        <q-separator dark vertical />
+        <NuxtLink v-slot="{ navigate }" custom to="/">
+          <q-btn stretch flat :label="$t('home')" no-caps @click="navigate()" />
+        </NuxtLink>
+        <q-separator dark vertical />
+        <NuxtLink v-slot="{ navigate }" custom to="/about">
           <q-btn
               stretch
               flat
-              :label="$t('youtube')"
+              :label="$t('about')"
               no-caps
-              @click="moveYoutube"
+              @click="navigate()"
           />
-          <q-separator dark vertical />
-          <NuxtLink v-slot="{ navigate }" custom to="/admin">
-            <q-btn
-                stretch
-                flat
-                :label="$t('admin')"
-                no-caps
-                @click="navigate()"
-            />
-          </NuxtLink>
-          <q-separator dark vertical />
-          <q-btn-dropdown stretch flat no-caps :label="selectedLanguageName">
-            <q-list padding dense>
-              <q-item
-                  v-for="{ code, name } in languages"
-                  :key="code"
-                  v-close-popup
-                  clickable
-                  :active="code === $i18n.locale"
-                  @click="$i18n.locale = code"
-              >
-                <q-item-section>
-                  <q-item-label>{{ name }}</q-item-label>
-                </q-item-section>
-              </q-item>
-            </q-list>
-          </q-btn-dropdown>
-          <q-separator dark vertical />
-          <NuxtLink v-if="!isAuthenticated" v-slot="{ navigate }" custom to="/login">
-            <q-btn
-                stretch
-                flat
-                :label="$t('login')"
-                no-caps
-                @click="navigate()"
-            />
-          </NuxtLink>
-          <NuxtLink v-else v-slot="{ navigate }" custom to="/">
-            <q-btn
-                stretch
-                flat
-                :label="$t('logout')"
-                no-caps
-                @click="signOut()"
-            />
-          </NuxtLink>
-<!--        </ClientOnly>-->
+        </NuxtLink>
+        <q-separator dark vertical />
+        <q-btn
+            stretch
+            flat
+            :label="$t('youtube')"
+            no-caps
+            @click="moveYoutube"
+        />
+        <q-separator dark vertical />
+        <NuxtLink v-slot="{ navigate }" custom to="/admin">
+          <q-btn
+              stretch
+              flat
+              :label="$t('admin')"
+              no-caps
+              @click="navigate()"
+          />
+        </NuxtLink>
+        <q-separator dark vertical />
+        <q-btn-dropdown stretch flat no-caps label="English">
+          <q-list padding dense>
+            <q-item v-close-popup clickable :to="localePath('/', 'en')">
+              <q-item-section>
+                <q-item-label>English</q-item-label>
+              </q-item-section>
+            </q-item>
+            <q-item v-close-popup clickable :to="switchLocalePath('ko')">
+              <q-item-section>
+                <q-item-label>한국어</q-item-label>
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </q-btn-dropdown>
+        <q-separator dark vertical />
+        <!-- <ClientOnly> -->
+        <NuxtLink
+            v-if="!isAuthenticated"
+            v-slot="{ navigate }"
+            custom
+            to="/login"
+        >
+          <q-btn
+              stretch
+              flat
+              :label="$t('login')"
+              no-caps
+              @click="navigate()"
+          />
+        </NuxtLink>
+        <q-btn
+            v-else
+            stretch
+            flat
+            :label="$t('logout')"
+            no-caps
+            @click="signOut()"
+        />
+        <!-- </ClientOnly> -->
       </q-toolbar>
     </q-header>
     <q-page-container :style="pageContainerStyle">
-<!--      <ClientOnly>-->
-        <q-banner v-if="isAuthenticated" class="bg-primary">
-          {{ authUser }}
-        </q-banner>
-        <slot></slot>
-<!--      </ClientOnly>-->
+      <!-- <ClientOnly> -->
+      <q-banner v-if="isAuthenticated" class="bg-primary text-white">
+        {{ authUser }}
+      </q-banner>
+      <!-- </ClientOnly> -->
+      <slot></slot>
     </q-page-container>
   </q-layout>
 </template>
 
 <script setup lang="ts">
+// const authUser = useAuthUser();
+// const isAuthenticated = useAuthenticated();
 const authStore = useAuthStore();
 const { user: authUser, isAuthenticated } = storeToRefs(authStore);
+
+// const { signOut } = useAuth();
 const { signOut } = authStore;
 const pageContainerStyle = computed(() => ({
   maxWidth: '1080px',
@@ -102,21 +108,7 @@ const moveYoutube = async () => {
   });
 };
 
-interface Language {
-  name: string;
-  code: 'en' | 'ko';
-}
-
-const languages = ref<Language[]>([
-  { name: 'English', code: 'en' },
-  { name: '한국어', code: 'ko' },
-]);
-
-const { locale } = useI18n();
-
-const selectedLanguageName = computed(
-    () => languages.value.find((lang) => lang.code === locale.value)?.name,
-);
-
-watch(locale, (val) => useCookie('locale').value = val);
+// const { locale } = useI18n();
+const localePath = useLocalePath();
+const switchLocalePath = useSwitchLocalePath();
 </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,7 +8,18 @@ export default defineNuxtConfig({
       'nuxt-quasar-ui',
       '@pinia/nuxt',
       '@pinia-plugin-persistedstate/nuxt',
+      '@nuxtjs/i18n',
   ],
+  i18n: {
+    locales: ['en', 'ko'], //URL 경로 접두사에 사용
+    defaultLocale: 'ko',  //Nuxt 페이지 및 라우팅을 위한 프로젝트의 기본 로케일
+    vueI18n: './i18n.config.ts',
+    detectBrowserLanguage: {
+      useCookie: true,
+      cookieKey: 'i18n_redirected',
+      redirectOn: 'root', // recommended
+    },
+  },
   quasar: {
     plugins: ['Notify'],
     config: {
@@ -18,12 +29,12 @@ export default defineNuxtConfig({
     },
   },
   imports: {
-    presets: [
-      {
-        from: 'vue-i18n',
-        imports: ['useI18n'],
-      },
-    ],
+    // presets: [
+    //   {
+    //     from: 'vue-i18n',
+    //     imports: ['useI18n'],
+    //   },
+    // ],
   },
   ssr: true,
   app: {
